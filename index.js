af235c82f5322e01f0f9022c2258997bbae87aee
Sheetal-beniwal <sheetalbeniwal60@gmail.com>
2026-02-01 08:25:25 +0530

    chore: initialize package.json with project metadata and dependencies

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to Database
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
+
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET','POST','PUT','DELETE'],
+        credentials: true,
+        allowedHeaders: ['Content-Type','Authorization'], 
+    }
+));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
